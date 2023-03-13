855d4e4b1d737ad2794b087a661eb17b2577eccc
rpenya <rpenya.mail@gmail.com>
2023-03-13 19:16:31 +0100

    create project

---
+++ rpenya-rickandmorty-challenge/rpenya-rickandmorty-challenge/rpenya_rickandmorty_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  rpenya_rickandmorty_challengeApp.swift
+//  rpenya-rickandmorty-challenge
+//
+//  Created by raulbot on 13/3/23.
+//
+
+import SwiftUI
+
+@main
+struct rpenya_rickandmorty_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
