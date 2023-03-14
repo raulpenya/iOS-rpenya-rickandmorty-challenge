880d6ff5791d7c5924273d519bdb5781385c9cc7
rpenya <rpenya.mail@gmail.com>
2023-03-14 18:35:38 +0100

    CharactersRemoteDataSourceTests done

--- rpenya-rickandmorty-challenge/Data/DataTests/Characters/CharactersRemoteDataSourceTests.swift
+++ rpenya-rickandmorty-challenge/Data/DataTests/Characters/CharactersRemoteDataSourceTests.swift
@@ -6,30 +6,54 @@
 //
 
 import XCTest
+@testable import Data
+import Combine
+import Domain
 
 final class CharactersRemoteDataSourceTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    let dataSource = CharactersRemoteDataSource()
+    var response: DataSourceResponse?
+    var errorResponse: Error?
+    var cancellableSet: Set<AnyCancellable> = []
+    
+    func test_getCharactersByPageNumber_success() {
+        //Given
+        let expectation = expectation(description: "test_session_executeTaskPublisher")
+        //When
+        dataSource.getCharactersByPageNumber(GetCharactersByPageNumberRequestValues(page: 1)).sink { [weak self] completion in
+            switch completion {
+            case .failure(let error):
+                self?.response = .error
+                self?.errorResponse = error
+            case .finished:
+                self?.response = .success
+            }
+            expectation.fulfill()
+        } receiveValue: { _ in }.store(in: &cancellableSet)
+        waitForExpectations(timeout: 5, handler: nil)
+        //Then
+        XCTAssertEqual(response, .success)
+        XCTAssertNil(errorResponse)
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func test_getCharacterById_success() {
+        //Given
+        let expectation = expectation(description: "test_session_executeTaskPublisher")
+        //When
+        dataSource.getCharacterById(GetCharacterByIdRequestValues(id: "812")).sink { [weak self] completion in
+            switch completion {
+            case .failure(let error):
+                self?.response = .error
+                self?.errorResponse = error
+            case .finished:
+                self?.response = .success
+            }
+            expectation.fulfill()
+        } receiveValue: { _ in }.store(in: &cancellableSet)
+        waitForExpectations(timeout: 5, handler: nil)
+        //Then
+        XCTAssertEqual(response, .success)
+        XCTAssertNil(errorResponse)
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
