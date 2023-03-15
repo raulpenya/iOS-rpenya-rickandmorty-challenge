12204ca6a08fa0dae5026c966137b6c38ceef709
rpenya <rpenya.mail@gmail.com>
2023-03-15 13:40:46 +0100

    CharacterListCellView added

---
+++ rpenya-rickandmorty-challenge/Mobile/CharactersList/UI/CharacterListCellView.swift
@@ -0,0 +1,23 @@
+//
+//  CharacterListCellView.swift
+//  rpenya-rickandmorty-challenge
+//
+//  Created by raulbot on 15/3/23.
+//
+
+import SwiftUI
+
+//https://www.avanderlee.com/swiftui/downloading-caching-images/
+
+struct CharacterListCellView: View {
+    var item: CharactersListItem
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+//struct CharacterListCellView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        CharacterListCellView()
+//    }
+//}
