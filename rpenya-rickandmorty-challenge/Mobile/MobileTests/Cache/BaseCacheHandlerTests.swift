ea81120f8fe834ebccc51b817b56d0e1e07d9c6e
rpenya <rpenya.mail@gmail.com>
2023-03-15 23:39:38 +0100

    cache handler tests done

--- rpenya-rickandmorty-challenge/Mobile/MobileTests/Cache/BaseCacheHandlerTests.swift
+++ rpenya-rickandmorty-challenge/Mobile/MobileTests/Cache/BaseCacheHandlerTests.swift
@@ -51,37 +51,6 @@ final class BaseCacheHandlerTests: XCTestCase {
         XCTAssertTrue(repository.loadObjectCalled)
         XCTAssertTrue(repository.saveObjectCalled)
     }
-}
-
-struct MockBaseCacheHandler: BaseCacheHandler {
     
-}
-
-class MockUserDefaultsRepository: KeyValueRepository {
-    
-    enum LoadObjectResponse {
-        case responseYoungTimestamp
-        case responseOldTimestamp
-        case responseNil
-    }
-    
-    var loadObjectResponse: LoadObjectResponse = .responseYoungTimestamp
-    var saveObjectCalled = false
-    var loadObjectCalled = false
-    
-    func saveObject(key: String, value: Any) {
-        saveObjectCalled = true
-    }
-    
-    func loadObject(key: String) -> Any? {
-        loadObjectCalled = true
-        switch loadObjectResponse {
-        case .responseYoungTimestamp:
-            return Date().timeIntervalSinceNow
-        case .responseOldTimestamp:
-            return Date().timeIntervalSinceNow-10000
-        case .responseNil:
-            return nil
-        }
-    }
+    struct MockBaseCacheHandler: BaseCacheHandler { }
 }
