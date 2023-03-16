ff1763ad58e8cdace6a8c2b47eca0a0c56611efc
rpenya <rpenya.mail@gmail.com>
2023-03-16 19:28:48 +0100

    PageInfoViewEntityTests done

--- rpenya-rickandmorty-challenge/Mobile/MobileTests/CharactersList/PageInfoViewEntityTests.swift
+++ rpenya-rickandmorty-challenge/Mobile/MobileTests/CharactersList/PageInfoViewEntityTests.swift
@@ -6,7 +6,34 @@
 //
 
 import XCTest
+@testable import rpenya_rickandmorty_challenge
 
 final class PageInfoViewEntityTests: XCTestCase {
-
+    func test_isLastPage_true() {
+        //Given
+        let pageInfo = MockPageInfoViewEntity.givenPageInfo(nextPage: nil)
+        //When
+        let result = pageInfo.isLastPage()
+        //Then
+        XCTAssertTrue(result)
+    }
+    
+    func test_isLastPage_false() {
+        //Given
+        let pageInfo = MockPageInfoViewEntity.givenPageInfo()
+        //When
+        let result = pageInfo.isLastPage()
+        //Then
+        XCTAssertFalse(result)
+    }
+    
+    func test_transformToUI() {
+        //Given
+        let pageInfo = MockPageInfo.givenPageInfo()
+        //When
+        let pageInfoUI = pageInfo.transformToUI()
+        //Then
+        XCTAssertEqual(pageInfo.nextPage, pageInfoUI.nextPage)
+        XCTAssertEqual(pageInfo.totalPages, pageInfoUI.totalPages)
+    }
 }
