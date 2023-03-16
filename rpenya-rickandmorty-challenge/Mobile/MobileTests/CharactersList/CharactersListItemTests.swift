395beea85401716ad0d55d1eeb83ad8e7e865338
rpenya <rpenya.mail@gmail.com>
2023-03-16 19:22:59 +0100

    CharactersListItemTests done

--- rpenya-rickandmorty-challenge/Mobile/MobileTests/CharactersList/CharactersListItemTests.swift
+++ rpenya-rickandmorty-challenge/Mobile/MobileTests/CharactersList/CharactersListItemTests.swift
@@ -6,7 +6,25 @@
 //
 
 import XCTest
+import SwiftUI
+@testable import rpenya_rickandmorty_challenge
 
 final class CharactersListItemTests: XCTestCase {
-
+    func test_getters() {
+        //Given
+        let character = MockCharacterThinViewEntity.givenCharacterThin1()
+        let item = CharactersListItem(character: character, onTapGesture: { _ in })
+        //When
+        let name = item.getName()
+        let url = item.getImageUrl()
+        let status = item.getStatusString()
+        let color = item.getStatusColor()
+        let gender = item.getGenderString()
+        //Then
+        XCTAssertEqual(name, character.name)
+        XCTAssertEqual(url?.absoluteString, character.imageUrl)
+        XCTAssertEqual(status, character.status.rawValue)
+        XCTAssertEqual(color, Color.green)
+        XCTAssertEqual(gender, character.gender.rawValue)
+    }
 }
