e8f8df866c7394fb7a953c62e8466855b24fd099
rpenya <rpenya.mail@gmail.com>
2023-03-16 20:01:43 +0100

    CharactersPagesViewEntityTests done

--- rpenya-rickandmorty-challenge/Mobile/MobileTests/CharactersList/CharactersPagesViewEntityTests.swift
+++ rpenya-rickandmorty-challenge/Mobile/MobileTests/CharactersList/CharactersPagesViewEntityTests.swift
@@ -6,7 +6,43 @@
 //
 
 import XCTest
+@testable import rpenya_rickandmorty_challenge
 
 final class CharactersPagesViewEntityTests: XCTestCase {
-
+    func test_addPage() {
+        //Given
+        let charactersPage = MockCharactersPage.givenCharactersPage()
+        let charactersPages = MockCharactersPagesViewEntity.givenCharactersPages()
+        //When
+        let newCharactersPages = charactersPages.add(newPage: charactersPage)
+        //Then
+        XCTAssertEqual(newCharactersPages.characters.count, charactersPages.characters.count+charactersPage.characters.count)
+    }
+    
+    func test_transformToListItems() {
+        //Given
+        let charactersPages = MockCharactersPagesViewEntity.givenCharactersPages()
+        //When
+        let items = charactersPages.transformToListItems(didReachListBottomAction: {}, onTapGesture: { _ in })
+        //Then
+        XCTAssertEqual(items.items.count, charactersPages.characters.count)
+    }
+    
+    func test_isListCompleted_true() {
+        //Given
+        var charactersPages = CharactersPagesViewEntity(currentPage: MockPageInfoViewEntity.givenPageInfo(nextPage: nil))
+        //When
+        let result = charactersPages.isListCompleted()
+        //Then
+        XCTAssertTrue(result)
+    }
+    
+    func test_isListCompleted_false() {
+        //Given
+        var charactersPages = CharactersPagesViewEntity(currentPage: MockPageInfoViewEntity.givenPageInfo())
+        //When
+        let result = charactersPages.isListCompleted()
+        //Then
+        XCTAssertFalse(result)
+    }
 }
