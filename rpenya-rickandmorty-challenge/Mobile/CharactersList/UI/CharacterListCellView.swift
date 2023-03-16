92897cdb3269461257147820e4ee80dd69cddf65
rpenya <rpenya.mail@gmail.com>
2023-03-16 18:46:36 +0100

    clean up code

--- rpenya-rickandmorty-challenge/Mobile/CharactersList/UI/CharacterListCellView.swift
+++ rpenya-rickandmorty-challenge/Mobile/CharactersList/UI/CharacterListCellView.swift
@@ -11,25 +11,26 @@ struct CharacterListCellView: View {
     static let width = (UIScreen.main.bounds.width/2)-6
     static let height = width
     static let padding = 8.0
-    
     var item: CharactersListItem
+    
     var body: some View {
-        //TODO: fix force URL
-        CharacterImageView(url: URL(string: item.getImageUrl())!)
-        .overlay(alignment: .bottomLeading) {
-            VStackLayout(alignment: .leading, spacing: 0) {
-                Text(item.getName()).modifier(TitleLightTextModifier())
-                    .padding([.top,.leading,.trailing], CharacterListCellView.padding)
-                HStack {
-                    Text(item.getGenderString()).modifier(SubtitleLightTextModifier())
-                    Text("|").modifier(SubtitleLightTextModifier())
-                    Text(item.getStatusString()).modifier(StatusTextModifier(color: item.getStatusColor()))
-                }.frame(maxWidth: .infinity, alignment: .leading).padding([.bottom,.leading,.trailing], CharacterListCellView.padding)
-            }.frame(maxWidth: .infinity)
-                .background {
-                    LinearGradient(gradient: Gradient(colors: [.black.opacity(0.8), .black.opacity(0)]), startPoint: .bottom, endPoint: .top)
-                }
-        }.cornerRadius(8)
+        if let url = item.getImageUrl() {
+            CharacterImageView(url: url)
+                .overlay(alignment: .bottomLeading) {
+                    VStackLayout(alignment: .leading, spacing: 0) {
+                        Text(item.getName()).modifier(TitleLightTextModifier())
+                            .padding([.top,.leading,.trailing], CharacterListCellView.padding)
+                        HStack {
+                            Text(item.getGenderString()).modifier(SubtitleLightTextModifier())
+                            Text("|").modifier(SubtitleLightTextModifier())
+                            Text(item.getStatusString()).modifier(StatusTextModifier(color: item.getStatusColor()))
+                        }.frame(maxWidth: .infinity, alignment: .leading).padding([.bottom,.leading,.trailing], CharacterListCellView.padding)
+                    }.frame(maxWidth: .infinity)
+                        .background {
+                            LinearGradient(gradient: Gradient(colors: [.black.opacity(0.8), .black.opacity(0)]), startPoint: .bottom, endPoint: .top)
+                        }
+                }.cornerRadius(8)
+        }
     }
 }
 
