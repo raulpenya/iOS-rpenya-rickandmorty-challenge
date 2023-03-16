c79ff4055842cc84a7b7b4f2d72e9436e6b68ddb
rpenya <rpenya.mail@gmail.com>
2023-03-16 12:19:14 +0100

    CharacterListCellView layout improved

--- rpenya-rickandmorty-challenge/Mobile/CharactersList/UI/CharacterListCellView.swift
+++ rpenya-rickandmorty-challenge/Mobile/CharactersList/UI/CharacterListCellView.swift
@@ -7,8 +7,6 @@
 
 import SwiftUI
 import Kingfisher
-//https://trailingclosure.com/device-motion-effect/
-//https://www.avanderlee.com/swiftui/downloading-caching-images/
 
 struct CharacterListCellView: View {
     static let width = (UIScreen.main.bounds.width/2)-6
@@ -21,22 +19,21 @@ struct CharacterListCellView: View {
         KFImage(URL(string: item.getImageUrl())!).placeholder({ _ in
             ProgressView()
         })
-            .aspectRatio(contentMode: .fill)
-            .frame(width: CharacterListCellView.width, height: CharacterListCellView.height, alignment: .center)
-            .clipped()
-            .overlay(alignment: .bottomLeading) {
-                VStackLayout(alignment: .leading, spacing: 0) {
-                    Text(item.getName()).modifier(TitleLightTextModifier())
-                        .padding([.top,.leading,.trailing], CharacterListCellView.padding)
-                    if let locationName = item.getLocationName() {
-                        Text(locationName).modifier(SubtitleLightTextModifier())
-                            .padding([.bottom,.leading,.trailing], CharacterListCellView.padding)
-                    }
-                }.frame(width: CharacterListCellView.width, alignment: .leading)
-                    .background {
-                        LinearGradient(gradient: Gradient(colors: [.black.opacity(0.7), .black.opacity(0)]), startPoint: .bottom, endPoint: .top)
-                    }
-            }.cornerRadius(8)
+        .resizable()
+        .aspectRatio(contentMode: .fit)
+        .overlay(alignment: .bottomLeading) {
+            VStackLayout(alignment: .leading, spacing: 0) {
+                Text(item.getName()).modifier(TitleLightTextModifier())
+                    .padding([.top,.leading,.trailing], CharacterListCellView.padding)
+                if let locationName = item.getLocationName() {
+                    Text(locationName).modifier(SubtitleLightTextModifier())
+                        .padding([.bottom,.leading,.trailing], CharacterListCellView.padding)
+                }
+            }.frame(width: CharacterListCellView.width, alignment: .leading)
+                .background {
+                    LinearGradient(gradient: Gradient(colors: [.black.opacity(0.7), .black.opacity(0)]), startPoint: .bottom, endPoint: .top)
+                }
+        }.cornerRadius(8)
     }
 }
 
