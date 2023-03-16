dc65d1ecbcb1460ecfa015ce34530fc10d767d43
rpenya <rpenya.mail@gmail.com>
2023-03-16 11:48:21 +0100

    CharacterDetailImage Cells added

---
+++ rpenya-rickandmorty-challenge/Mobile/CharacterDetail/UI/CharacterDetailImageCellView.swift
@@ -0,0 +1,20 @@
+//
+//  CharacterDetailImageCellView.swift
+//  rpenya-rickandmorty-challenge
+//
+//  Created by raulbot on 16/3/23.
+//
+
+import SwiftUI
+
+struct CharacterDetailImageCellView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CharacterDetailImageCellView_Previews: PreviewProvider {
+    static var previews: some View {
+        CharacterDetailImageCellView()
+    }
+}
---
+++ rpenya-rickandmorty-challenge/Mobile/CharacterDetail/UI/CharacterDetailInfoCellView.swift
@@ -0,0 +1,20 @@
+//
+//  CharacterDetailInfoCellView.swift
+//  rpenya-rickandmorty-challenge
+//
+//  Created by raulbot on 16/3/23.
+//
+
+import SwiftUI
+
+struct CharacterDetailInfoCellView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CharacterDetailInfoCellView_Previews: PreviewProvider {
+    static var previews: some View {
+        CharacterDetailInfoCellView()
+    }
+}
