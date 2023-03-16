70fc3b4d8691d7e21bf4472d4cf0ed65fef206f5
rpenya <rpenya.mail@gmail.com>
2023-03-16 20:36:02 +0100

    CharacterDetailViewModelFlowTests done

--- rpenya-rickandmorty-challenge/Mobile/MobileTests/CharacterDetail/CharacterDetailViewModelFlowTests.swift
+++ rpenya-rickandmorty-challenge/Mobile/MobileTests/CharacterDetail/CharacterDetailViewModelFlowTests.swift
@@ -6,7 +6,45 @@
 //
 
 import XCTest
+@testable import rpenya_rickandmorty_challenge
 
 final class CharacterDetailViewModelFlowTests: XCTestCase {
-
+    func test_loadData_flow() {
+        //Given
+        let model = MockCharacterDetailViewModel.getModel()
+        //When
+        model.loadData()
+        //Then
+        XCTAssertTrue(model.updateViewCalled)
+        XCTAssertTrue(model.getCharacterCalled)
+    }
+    
+    func test_refreshData_flow() {
+        //Given
+        let model = MockCharacterDetailViewModel.getModel()
+        //When
+        model.refreshData()
+        //Then
+        XCTAssertTrue(model.loadDataCalled)
+        XCTAssertTrue(model.updateViewCalled)
+        XCTAssertTrue(model.getCharacterCalled)
+    }
+    
+    func test_dismissButtonPressed_flow() {
+        //Given
+        let model = MockCharacterDetailViewModel.getModel()
+        //When
+        model.dismissButtonPressed()
+        //Then
+        XCTAssertTrue(model.dismissCharacterDetail)
+    }
+    
+    func test_receiveResult_flow() {
+        //Given
+        let model = MockCharacterDetailViewModel.getModel()
+        //When
+        model.receiveResult(MockCharacter.givenCharacter1())
+        //Then
+        XCTAssertTrue(model.updateViewCalled)
+    }
 }
