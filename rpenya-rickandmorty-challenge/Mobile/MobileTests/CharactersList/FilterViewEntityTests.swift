d827d9f96c8139ab2995ac03570987182af3c0b3
rpenya <rpenya.mail@gmail.com>
2023-03-17 20:41:40 +0100

    FilterViewEntityTests done

--- rpenya-rickandmorty-challenge/Mobile/MobileTests/CharactersList/FilterViewEntityTests.swift
+++ rpenya-rickandmorty-challenge/Mobile/MobileTests/CharactersList/FilterViewEntityTests.swift
@@ -6,7 +6,45 @@
 //
 
 import XCTest
+@testable import rpenya_rickandmorty_challenge
 
 final class FilterViewEntityTests: XCTestCase {
-
+    func test_transformToAnyItem() {
+        //Given
+        let filter = MockFilterViewEntity.givenFilterViewEntity1()
+        //When
+        let anyItem = filter.transformToAnyItem(onTapGesture: { _ in })
+        //Then
+        XCTAssertEqual((anyItem.item as! CharactersListFilterItem).filter, filter)
+    }
+    
+    func test_transformToCharactersListFilterItem() {
+        //Given
+        let filter = MockFilterViewEntity.givenFilterViewEntity1()
+        //When
+        let item = filter.transformToCharactersListFilterItem(onTapGesture: { _ in })
+        //Then
+        XCTAssertEqual(item.filter, filter)
+        XCTAssertEqual(item.id, filter.text)
+    }
+    
+    func test_transformToQueryParam_notNil() {
+        //Given
+        let filter = MockFilterViewEntity.givenFilterViewEntity2()
+        //When
+        let queryParam = filter.transformToQueryParam()
+        //Then
+        XCTAssertNotNil(queryParam)
+        XCTAssertEqual(queryParam, filter.text)
+    }
+    
+    func test_transformToQueryParam_nil() {
+        //Given
+        let filter = MockFilterViewEntity.givenFilterViewEntity1()
+        //When
+        let queryParam = filter.transformToQueryParam()
+        //Then
+        XCTAssertNil(queryParam)
+        XCTAssertEqual(FiltersViewEntity.noFilterName, filter.text)
+    }
 }
